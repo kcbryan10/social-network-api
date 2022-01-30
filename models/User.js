059dc939a61e6229745e0d509f991ff583797c0e
kcbryan <kcbryan10@gmail.com>
2022-01-30 12:13:23 -0600

    added user model

--- models/User.js
+++ models/User.js
@@ -1,1 +1,33 @@
+const {Schema, model} = require('mongoose');
+const { schema } = require('./Thought');
+
+const userSchema = new Schema({
+    username: {
+        type: String,
+        required: true, 
+        trim: true,
+        unique: true
+    },
+    email: {
+        type: String,
+        required: true, 
+        unique: true,
+        match: [/.+@.+\..+/]
+    },
+    thoughts: [{type: schema.Types.ObjectId, ref: 'Thought'}],
+    friends: [{type: schema.Types.ObjectId, ref: 'User'}]
+},
+{
+    toJSON: {
+        virtuals: true
+    },
+    id: false
+});
+
+userSchema.virtual('friendCount').get(function() {
+    return this.friends.lenth;
+});
+
+const User = model('User', userSchema);
+
 module.exports = User;
